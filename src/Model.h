7a3770de7dae9ef887d0c601cbbbf1ddc44aae70
liza kozlova <liza903kozlova@yandex.ru>
2026-03-05 12:26:27 +0300

    structure

---
+++ src/Model.h
@@ -0,0 +1,8 @@
+//
+// Created by liza9 on 05.03.2026.
+//
+
+#ifndef MYLETTERBOX_MODEL_H
+#define MYLETTERBOX_MODEL_H
+
+#endif //MYLETTERBOX_MODEL_H
---
+++ src/main.cpp
@@ -0,0 +1,3 @@
+//
+// Created by liza9 on 05.03.2026.
+//
