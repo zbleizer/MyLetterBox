c0c79eeaeb40c6a79b126b6716d4d10d24804e05
liza kozlova <liza903kozlova@yandex.ru>
2026-03-07 13:44:31 +0300

    review

--- src/Model.h
+++ src/Model.h
@@ -2,6 +2,26 @@
 #define MYLETTERBOX_MODEL_H
 #include <string>
 #include <algorithm>
+#include <stdexcept>
+#include <iostream>
+
+class Review {
+    std::string author;
+    std::string text;
+    int score;
+public:
+    Review(std::string a, std::string t, int s) : author(std::move(a)), text(std::move(t)), score(s) {
+        if (score < 1 || score > 10) {
+            throw std::invalid_argument("Score must be 1-10");
+        }
+    }
+    void print() const {
+        std::cout << "  [" << score << "/10] " << author << ": " << text << "\n";
+    }
+    int getScore() const {
+        return score;
+    }
+};
 
 class Media {
 protected:
