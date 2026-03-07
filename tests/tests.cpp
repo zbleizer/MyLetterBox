8fffeb654db5eb1639f1e01b34e4bb629e1ab814
liza kozlova <liza903kozlova@yandex.ru>
2026-03-08 02:45:09 +0300

    tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -17,21 +17,33 @@ TEST(ReviewTest, NeedException) {
 
 
 TEST(MovieTest, ShouldPass_Init) {
-    Movie m("Forrest Gump", 1994, "Robert Zemeckis", 142);
+    Movie m("Forrest Gump", "Drama", "https://forrestgump.com", {"fast", "speed"}, 142);
 
     EXPECT_EQ(m.getTitle(), "Forrest Gump");
-    EXPECT_EQ(m.getYear(), 1994);
+    EXPECT_EQ(m.getGenres(), "Drama");
+    EXPECT_EQ(m.getType(), "Movie");
     EXPECT_EQ(m.getDuration(), 142);
     EXPECT_EQ(m.getAveRating(), 0.0);
+
+    EXPECT_NO_THROW(m.info());
 }
 
+
 TEST(MovieTest, ShouldPass_AveRate) {
-    Movie m("Forrest Gump", 1994, "Robert Zemeckis", 142);
+    Movie m("Forrest Gump", "Drama", "https://forrestgump.com", {"fast", "speed"}, 142);
+
 
     m.addReview(Review("liza1", "amazing", 10));
     EXPECT_EQ(m.getAveRating(), 10.0);
     m.addReview(Review("liza2", "norm", 5));
     EXPECT_EQ(m.getAveRating(), 7.5);
+}
+
+TEST(MovieTest, ShouldPass_Keywords) {
+    Movie m("Forrest Gump", "Drama", "https://forrestgump.com", {"fast", "speed"}, 142);
+    EXPECT_TRUE(m.haveKeyword("Fast"));
+    EXPECT_TRUE(m.haveKeyword("SPEED"));
+    EXPECT_FALSE(m.haveKeyword("gg"));
 }
 
 int main(int argc, char **argv) {
